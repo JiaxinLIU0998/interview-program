57cba137aa90194b1f8388ebe7b106d47a0476d3
JiaxinLIU0998 <71937749+JiaxinLIU0998@users.noreply.github.com>
2022-12-15 22:02:12 +0800

    Update inference.py

--- part1/SSSD/inference.py
+++ part1/SSSD/inference.py
@@ -1,6 +1,4 @@
 import os
-os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-os.environ["CUDA_VISIBLE_DEVICES"]="3"
 import argparse
 import json
 import numpy as np
@@ -8,10 +6,10 @@ import tensorflow as tf
 
 from utils.util import get_mask_mnr, get_mask_bm, get_mask_rm
 from utils.util import find_max_epoch, sampling, calc_diffusion_hyperparams
-
 from imputers.SSSDS4Imputer import SSSDS4Imputer
 
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 from statistics import mean
 
 
@@ -20,10 +18,8 @@ def generate(output_directory,
              ckpt_path,
              data_path,
              ckpt_iter,
-             use_model,
              masking,
-             missing_k,
-             only_generate_missing):
+             missing_k):
     
     """
     Generate data based on ground truth 
@@ -35,9 +31,7 @@ def generate(output_directory,
     ckpt_iter (int or 'max'):         the pretrained checkpoint to be loaded; 
                                       automitically selects the maximum iteration if 'max' is selected
     data_path (str):                  path to dataset, numpy array.
-    use_model (int):                  0:DiffWave. 1:SSSDSA. 2:SSSDS4.
     masking (str):                    'mnr': missing not at random, 'bm': black-out, 'rm': random missing
-    only_generate_missing (int):      0:all sample diffusion.  1:only apply diffusion to missing portions of the signal
     missing_k (int)                   k missing time points for each channel across the length.
     """
 
@@ -55,23 +49,14 @@ def generate(output_directory,
 
             
     # predefine model
-    if use_model == 0:
-        net = DiffWaveImputer(**model_config)
-    elif use_model == 1:
-        net = SSSDSAImputer(**model_config)
-    elif use_model == 2:
-        net = SSSDS4Imputer(**model_config)
-    else:
-        print('Model chosen not available.')
-    
-
+    net = SSSDS4Imputer(**model_config)
     
     # load checkpoint
     ckpt_path = os.path.join(ckpt_path, local_path)
     if ckpt_iter == 'max':
         ckpt_iter = find_max_epoch(ckpt_path)
-    try:
-        net.restore(output_directory +'/{}/{}.ckpt'.format(ckpt_iter, ckpt_iter)).expect_partial()
+    try: 
+        net.load_weights(output_directory +'/{}/{}.ckpt'.format(ckpt_iter, ckpt_iter))
         print('Successfully loaded model at iteration {}'.format(ckpt_iter))
     except:
         raise Exception('No valid model found')
@@ -79,18 +64,20 @@ def generate(output_directory,
         
         
     ### Custom data loading and reshaping ###
-    
+    batch_size, masking, missing_k = train_config['batch_size'],train_config['masking'],train_config['missing_k']
     testing_data = np.load(trainset_config['test_data_path'])
-    testing_data = testing_data[312:,::]
+    drop_data = testing_data.shape[0]%batch_size
+    testing_data = testing_data[drop_data:,:,:]
+  
     testing_data = np.transpose(testing_data,(0,2,1))
-    testing_data = np.split(testing_data, 17, 0) # 160,50,100,14
+    testing_data = np.split(testing_data, 17, 0) 
     testing_data = np.array(testing_data)
+    testing_data = tf.convert_to_tensor(testing_data)
     
     print('Data loaded')
 
     all_mse = []
 
-    
     for i, batch in enumerate(testing_data):
         if masking == 'rm':
             mask_T = get_mask_rm(batch[0], missing_k)
@@ -98,11 +85,11 @@ def generate(output_directory,
             mask_T = get_mask_mnr(batch[0], missing_k)
         elif masking == 'bm':
             mask_T = get_mask_bm(batch[0], missing_k)
-        
-        mask = np.transpose(mask_T, [1,0]) 
-        mask = np.repeat(np.expand_dims(mask, axis=0), repeats=batch.shape[0],axis = 0)
-    
-        batch = np.transpose(batch, [0,2,1]) 
+            
+        mask = tf.transpose(transposed_mask, [1,0]) 
+        mask =  tf.repeat(tf.expand_dims(mask, axis=0), repeats=batch.shape[0],axis = 0)
+        loss_mask = tf.cast(tf.where(mask==0,1,0),dtype=tf.bool) 
+        batch = tf.transpose(batch, [0,2,1]) 
         
         sample_length = batch.shape[2]
         sample_channels = batch.shape[1]
@@ -111,25 +98,26 @@ def generate(output_directory,
                                    diffusion_hyperparams,
                                    cond=batch,
                                    mask=mask,
-                                   only_generate_missing=only_generate_missing)
+                                   z = batch,
+                                   loss_mask = loss_mask)
 
 
-        print('generated {} utterances of random_digit at iteration {} '.format(num_samples,ckpt_iter, ))
+        print('generated {} utterances of random_digit at iteration {} '.format(num_samples,ckpt_iter ))
 
-        bool_mask = mask.astype(bool) 
         mse = mean_squared_error(generated_audio[bool_mask], batch[bool_mask])
+        mae = mean_absolute_error(generated_audio[bool_mask], batch[bool_mask])
         all_mse.append(mse)
+        all_mae.append(mae)
     
     print('Total MSE:', mean(all_mse))
+    print('Total MAE:', mean(all_mae))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', type=str, default='./config/config_SSSDS4_ptx.json',
+    parser.add_argument('-c', '--config', type=str, default='./config/config_SSSDS4_bm_ptx.json',
                         help='JSON file for configuration')
-    parser.add_argument('-ckpt_iter', '--ckpt_iter', default='max',
-                        help='Which checkpoint to use; assign a number or "max"')
-    parser.add_argument('-n', '--num_samples', type=int, default=500,
+    parser.add_argument('-n', '--num_samples', type=int, default=10,
                         help='Number of utterances to be generated')
     args = parser.parse_args()
     
@@ -158,18 +146,11 @@ if __name__ == "__main__":
         **diffusion_config)  # dictionary of all diffusion hyperparameters
 
     global model_config
-    if train_config['use_model'] == 0:
-        model_config = config['wavenet_config']
-    elif train_config['use_model'] == 1:
-        model_config = config['sashimi_config']
-    elif train_config['use_model'] == 2:
-        model_config = config['wavenet_config']
+    model_config = config['wavenet_config']
 
     generate(**gen_config,
-             ckpt_iter=args.ckpt_iter,
+             ckpt_iter=config["train_config"]['ckpt_iter'],
              num_samples=args.num_samples,
-             use_model=train_config["use_model"],
              data_path=trainset_config["test_data_path"],
              masking=train_config["masking"],
-             missing_k=train_config["missing_k"],
-             only_generate_missing=train_config["only_generate_missing"])
+             missing_k=train_config["missing_k"])
