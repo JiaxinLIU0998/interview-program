a79227724fd46b978a91c78ccfb1e315c2dd6a2a
JiaxinLIU0998 <71937749+JiaxinLIU0998@users.noreply.github.com>
2022-12-18 21:53:58 +0800

    Update CSDI_train.py

--- part1/CSDI/CSDI_train.py
+++ part1/CSDI/CSDI_train.py
@@ -1,19 +1,25 @@
 import numpy as np
 from imputers.CSDI import CSDIImputer
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
 
-CSDI_model = CSDIImputer(1)
+CSDI_model = CSDIImputer(0)
 
 CSDI_model.train(
     #training_data,
     masking ='rm',
     missing_ratio_or_k = 0.2,
     trainset_path = './datasets/train_ptbxl_248.npy',
+    valset_path = './datasets/val_ptbxl_248.npy',
     testset_path = './datasets/test_ptbxl_248.npy',
     epochs = 200,
     samples_generate = 10,
-    path_save = "./results/train_ptbxl_248/CSDI",
+    path_save = "/home/root/jpm/SSSD/tf/upload_CSDI/results/train_ptbxl_248/CSDI/",
     batch_size = 8,
     lr = 1.0e-3,
     layers = 4,
