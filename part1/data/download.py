f0e10f93b75d6b640fbf0b87bdd0d286f4ca1bf7
JiaxinLIU0998 <71937749+JiaxinLIU0998@users.noreply.github.com>
2022-12-25 18:31:31 +0800

    Update download.py

--- part1/data/download.py
+++ part1/data/download.py
@@ -44,7 +44,7 @@ def data_download(index_name):
     Parameters:
     index_name(str):       choose from ['HSI','DJ30','EU50']
     """
-    index=pd.read_csv('./datasets/'+index_name+'/' + index_name + '.csv',header =None)
+    index=pd.read_csv(index_name + '.csv',header =None)
     
     if index_name == 'HSI':
         index0 = list(index.iloc[:,0])
@@ -67,22 +67,21 @@ def data_download(index_name):
         except:
             print('Data download for '+ticker+' fails, please double check')
 
-    all_data.to_csv('./datasets/'+index_name+'/'+index_name+'_data.csv')
+    all_data.to_csv(index_name+'_data.csv')
     print('Finished downloaded data for '+index_name+' and save in '+index_name+'_data.csv')
 
     
 
-def process(index_name):
-    """
-    process stock data to generate model input array, and the historical mask pattern
+def generate(index_name,data_path,time_stamp):
+    ############################################################################
+    #               generate observation  with missing value                   #
+    ############################################################################
 
-    Parameters:
-    index_name(str):                choose from ['HSI','DJ30','EU50']
-    """
-    
+
     # insert the missing values in the downloaded dataframe 
-    INDEX_data = pd.read_csv('./datasets/'+index_name+'/'+index_name+'_data.csv')
+    INDEX_data = pd.read_csv(index_name+'_data.csv')
     tickers = list(set(INDEX_data['ticker'].tolist()))
+    num_of_tickers = len(tickers)
     INDEX_data = INDEX_data.drop(columns=['Unnamed: 0'])
     delta = timedelta(days=1)
 
@@ -90,7 +89,7 @@ def process(index_name):
     for i in range(INDEX_data.shape[0]):
         datetime_list.append(datetime.strptime(INDEX_data['Date'][i], '%Y-%m-%d'))
     INDEX_data['datetime'] = pd.Series(datetime_list)
-    
+
     for ticker in tickers:
         INDEX_time_stamp = INDEX_data[INDEX_data['ticker'] == ticker]['datetime'].tolist()
         start = INDEX_time_stamp[0]
@@ -101,7 +100,7 @@ def process(index_name):
             if start not in INDEX_time_stamp and int(start.weekday()) != 5 and int(start.weekday()) != 6:  
                 holiday_INDEX.append(start)
             start = start + delta
-            
+
         if ticker[-2:] == 'DE':
             holiday_INDEX_DE = holiday_INDEX
 
@@ -113,133 +112,173 @@ def process(index_name):
             count += 1
         print('number of holiday days for '+ str(ticker) + ' in ' + str(index_name)+ ' index : ' + str(len(holiday_INDEX)))                
         assert len(holiday_INDEX) == count
-    #INDEX_data.to_csv('./'+ str(index_name) +'_data_withmissing'+'.csv')
-    
-    if index_name == 'EU50':
-        holiday_INDEX = holiday_INDEX_DE
-    
-    #generate mask patterns according to historical patterns
-    mask = []
-    count = 0
-    datetime_series = INDEX_data['datetime'].tolist()
-    month = 0
-    while datetime.strptime('2012-12-03', '%Y-%m-%d') + timedelta(days=month*28) < datetime_series[-1]:
-        start = datetime.strptime('2012-12-03', '%Y-%m-%d') + timedelta(days=month*28)
-        mask_month = []
-        for i in range(28):
-            if start + timedelta(days=i) not in holiday_INDEX and start + timedelta(days=i) in INDEX_data['datetime'].tolist():
-                mask_month.append(0)
-            elif start + timedelta(days=i) in holiday_INDEX and start + timedelta(days=i) in INDEX_data['datetime'].tolist():
-                mask_month.append(1)
-                count += 1
-            else:
-                continue
-        mask.append(mask_month)
-        month += 1
 
-    assert count == len(holiday_INDEX) 
 
-    for add_zero in range(20 - len(mask[-1])):
-        mask[-1].append(0)
-        
-    mask = np.expand_dims(mask,axis=-1)
-    mask = np.repeat(mask,6,axis=2)
+
+    ##########################################################################################
+    #          generate dataframe with column indexs == tickers, row index == date           #
+    ##########################################################################################
+
+    test_time = ['2016-12-01','2017-11-30','2021-11-30','2022-11-30']
+    val_time = ['2018-12-01','2019-11-30']
+
+    tickers = list(set((INDEX_data['ticker'].tolist())))
+    df_adjclose = pd.DataFrame(columns=tickers)
+    for ticker in tickers:
+        df_adjclose[ticker] = (INDEX_data[INDEX_data['ticker']==ticker]['Adj Close']).to_numpy()
+
+    df_adjclose['Date'] = INDEX_data['Date'].tolist()[: df_adjclose.shape[0]]   
+    df_adjclose = df_adjclose.reset_index()
+    df_adjclose = df_adjclose.drop(columns = ['index'])
+    df_adjclose['Date'] = pd.to_datetime(df_adjclose['Date'])
+
+    ###########################################################################################
+    # use the mean/std of training_validatin set to normalize data                            #
+    ###########################################################################################
+    train_all = pd.concat([df_adjclose[df_adjclose['Date']<datetime.strptime(test_time[0], '%Y-%m-%d')].reset_index(),
+                           df_adjclose[datetime.strptime(test_time[1],'%Y-%m-%d')<df_adjclose['Date']][df_adjclose['Date']<datetime.strptime(test_time[2], '%Y-%m-%d')].reset_index()
+                          ])
+
+    mean_std = {}
+    for i in train_all.columns.tolist():
+        if i != 'index' and i != 'Date':
+            mean_std[i] = [np.mean(train_all[i]),np.std(train_all[i])]
+
+    for i in df_adjclose.columns:
+        if i in mean_std.keys():
+            df_adjclose[i] = (df_adjclose[i] - mean_std[i][0])/mean_std[i][1]
+
+    with open(data_path +'/'+index_name+'_mean_std.pickle', 'wb') as handle:
+        pickle.dump(mean_std, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+    ###########################################################################################
+    # generate training/valisation/testing set (.npy)                                         #
+    ###########################################################################################
+
+
+    train1 = df_adjclose[df_adjclose['Date']<datetime.strptime(test_time[0], '%Y-%m-%d')].reset_index()
+    test1 = df_adjclose[datetime.strptime(test_time[0], '%Y-%m-%d')<=df_adjclose['Date']][df_adjclose['Date']<=datetime.strptime(test_time[1],'%Y-%m-%d')].reset_index()
+    train2 = df_adjclose[datetime.strptime(test_time[1],'%Y-%m-%d')<df_adjclose['Date']][df_adjclose['Date']<datetime.strptime(val_time[0], '%Y-%m-%d')].reset_index()
+    val1 = df_adjclose[datetime.strptime(val_time[0], '%Y-%m-%d')<=df_adjclose['Date']][df_adjclose['Date']<=datetime.strptime(val_time[1], '%Y-%m-%d')].reset_index()
+    train3 = df_adjclose[datetime.strptime(val_time[1], '%Y-%m-%d')<df_adjclose['Date']][df_adjclose['Date']<=datetime.strptime(test_time[2], '%Y-%m-%d')].reset_index()
+    test2 = df_adjclose[datetime.strptime(test_time[2], '%Y-%m-%d')<df_adjclose['Date']][df_adjclose['Date']<=datetime.strptime(test_time[3], '%Y-%m-%d')].reset_index()
+
+
+    for i in range(train1.shape[0] - time_stamp + 1):
+        if i == 0:
+            train = train1.loc[i:i+time_stamp-1,tickers].to_numpy()
+        else:
+            train = np.concatenate([train,train1.loc[i:i+time_stamp-1,tickers].to_numpy()])       
+    for i in range(train2.shape[0] - time_stamp + 1):
+        train = np.concatenate([train,train2.loc[i:i+time_stamp-1,tickers].to_numpy()])    
+    for i in range(train3.shape[0] - time_stamp + 1):
+        train = np.concatenate([train,train3.loc[i:i+time_stamp-1,tickers].to_numpy()])
+
+    train = np.reshape(train,[-1,time_stamp,num_of_tickers])
+    np.save(data_path+'/'+index_name+'_train.npy',train)
+
+
+    for i in range(val1.shape[0] - time_stamp + 1):
+        if i == 0:
+            val = val1.loc[i:i+time_stamp-1,tickers].to_numpy()
+        else:
+            val = np.concatenate([val,val1.loc[i:i+time_stamp-1,tickers].to_numpy()])   
+    val = np.reshape(val,[-1,time_stamp,num_of_tickers])   
+    np.save(data_path+'/'+index_name+'_val.npy',val)
+
+    for i in range(test1.shape[0]//time_stamp):
+        start = i*time_stamp
+        end = (i+1)*time_stamp
+        if i == 0:
+            test = test1.loc[start:end-1,tickers].to_numpy()
+        else:
+            test = np.concatenate([test,test1.loc[start:end-1,tickers].to_numpy()]) 
+
+    for i in range(test2.shape[0]//time_stamp):
+        start = i*time_stamp
+        end = (i+1)*time_stamp
+        test = np.concatenate([test,test2.loc[start:end-1,tickers].to_numpy()]) 
+
+    test = np.reshape(test,[-1,time_stamp,num_of_tickers]) 
+    np.save(data_path+'/'+index_name+'_test.npy',test)
+
+    ###########################################################################################
+    # generate mask                                                                           #
+    ###########################################################################################
+
+    for i in range(train1.shape[0]//time_stamp):
+        start = i*time_stamp
+        end = (i+1)*time_stamp
+        if i == 0:
+            mask = np.where(train1.loc[start:end-1,tickers].to_numpy(),1,0)
+        else:
+            mask = np.concatenate([mask,np.where(np.isnan(train1.loc[start:end-1,tickers].to_numpy()),0,1)]) 
+
+    for i in range(train2.shape[0]//time_stamp):
+        start = i*time_stamp
+        end = (i+1)*time_stamp
+        mask = np.concatenate([mask,np.where(np.isnan(train2.loc[start:end-1,tickers].to_numpy()),0,1)]) 
+
+    for i in range(val1.shape[0]//time_stamp):
+        start = i*time_stamp
+        end = (i+1)*time_stamp
+        mask = np.concatenate([mask,np.where(np.isnan(val1.loc[start:end-1,tickers].to_numpy()),0,1)]) 
+
+    for i in range(train3.shape[0]//time_stamp):
+        start = i*time_stamp
+        end = (i+1)*time_stamp
+        mask = np.concatenate([mask,np.where(np.isnan(train3.loc[start:end-1,tickers].to_numpy()),0,1)]) 
+
+    mask = np.reshape(mask,[-1,time_stamp,num_of_tickers])   
     mask = np.unique(mask,axis=0)
 
+    index_delete = []
     for i in range(mask.shape[0]):
-        if mask[i].sum() == 0:
-            index = i
-    mask = np.delete(mask,index, 0)
-    mask = np.where(mask==0,1,0)
-    
-    print('generating '+str(len(mask))+' patterns for index '+str(index_name))   
-    np.save('./datasets/'+index_name+'/'+index_name+'_mask.npy',np.array(mask))
-   
-    
-    # data normalization 
-    #INDEX_data = INDEX_data.drop(columns=['Unnamed: 0'])
-    ticker_list = list(set(INDEX_data['ticker'].tolist()))
-    
-    INDEX_data_normed = pd.DataFrame({'Date':[],'Open':[],'High':[],'Low':[],'Close':[],'Adj Close':[],'Volume':[],'ticker':[],'datetime':[]})
-    for ticker in ticker_list:
-        subdf = INDEX_data[INDEX_data['ticker'] == ticker].reset_index()
-        subdf = subdf.drop(columns=['index'])
-        test = pd.DataFrame({'Date':subdf['Date'],'Open':preprocess(subdf['Open']),'High':preprocess(subdf['High']),
-                             'Low':preprocess(subdf['Low']),'Close':preprocess(subdf['Close']),
-                             'Adj Close':preprocess(subdf['Adj Close']), 'Volume':preprocess(subdf['Volume']),
-                             'ticker':subdf['ticker'],'datetime':subdf['datetime']})
-        INDEX_data_normed = pd.concat([INDEX_data_normed,test])
-        
-    
-    #generate training/validation/test array
-    tickers = list(set(INDEX_data_normed['ticker'].to_list()))
-
-    
-    single = INDEX_data_normed[INDEX_data_normed['ticker']==tickers[0]].reset_index()
-    single = single.drop(columns=['index'])
-    total_length = single.shape[0]
-    train_length = int(total_length*0.7)
-    val_length = int(total_length*0.1)
-    test_length = int(total_length*0.2)
-    
-    
-    train_data = single.loc[0:20-1,['Open','High','Low','Close','Adj Close','Volume']].to_numpy()
-    for i in range(1,int(train_length//10)-1):
-        start_index = i*10
-        train_data = np.vstack((train_data,single.loc[start_index:start_index+20-1,['Open','High','Low','Close','Adj Close','Volume']].to_numpy()))
-        
-    val_data = single.loc[(int(train_length//10)-1)*10:(int(train_length//10)-1)*10+20-1,['Open','High','Low','Close','Adj Close','Volume']].to_numpy()
-    for i in range(int(train_length//10),int((train_length+val_length)//10)-1):
-        start_index = i*10
-        val_data = np.vstack((val_data,single.loc[start_index:start_index+20-1,['Open','High','Low','Close','Adj Close','Volume']].to_numpy()))
-        
-    test_data = single.loc[(int((train_length+val_length)//10)-1)*10:(int((train_length+val_length)//10)-1)*10+20-1,['Open','High','Low','Close','Adj Close','Volume']].to_numpy()
-    for i in range(int((train_length+val_length)//10),int((train_length+val_length+test_length)//10)-1):
-        start_index = i*10
-        test_data = np.vstack((test_data,single.loc[start_index:start_index+20-1,['Open','High','Low','Close','Adj Close','Volume']].to_numpy()))
-    
-    total_train = train_data
-    total_val = val_data
-    total_test = test_data
+        if mask[i].sum() == (time_stamp*num_of_tickers):
+            index_delete.append(i)
+    if len(index_delete)!= 0:
+        mask = np.delete(mask,index_delete, 0)
 
 
-    for ticker in tickers[1:]:
-        single = INDEX_data_normed[INDEX_data_normed['ticker']==ticker].reset_index()
-        single = single.drop(columns=['index'])
-        total_length = single.shape[0]
-        
-        train_length = int(total_length*0.7)
-        val_length = int(total_length*0.1)
-        test_length = int(total_length*0.2)
 
-        train_data = single.loc[0:20-1,['Open','High','Low','Close','Adj Close','Volume']].to_numpy()
-        for i in range(1,int(train_length//10)-1):
-            start_index = i*10
-            train_data = np.vstack((train_data,single.loc[start_index:start_index+20-1,['Open','High','Low','Close','Adj Close','Volume']].to_numpy()))
+    np.save(data_path+'/'+index_name+'_mask_train.npy',mask)
 
-        val_data = single.loc[(int(train_length//10)-1)*10:(int(train_length//10)-1)*10+20-1,['Open','High','Low','Close','Adj Close','Volume']].to_numpy()
-        for i in range(int(train_length//10),int((train_length+val_length)//10)-1):
-            start_index = i*10
-            val_data = np.vstack((val_data,single.loc[start_index:start_index+20-1,['Open','High','Low','Close','Adj Close','Volume']].to_numpy()))
 
-        test_data = single.loc[(int((train_length+val_length)//10)-1)*10:(int((train_length+val_length)//10)-1)*10+20-1,['Open','High','Low','Close','Adj Close','Volume']].to_numpy()
-        for i in range(int((train_length+val_length)//10),int((train_length+val_length+test_length)//10)-1):
-            start_index = i*10
-            test_data = np.vstack((test_data,single.loc[start_index:start_index+20-1,['Open','High','Low','Close','Adj Close','Volume']].to_numpy()))
+    for i in range(test1.shape[0]//time_stamp):
+        start = i*time_stamp
+        end = (i+1)*time_stamp
+        if i == 0:
+            mask_test = np.where(np.isnan(test1.loc[start:end-1,tickers].to_numpy()),0,1)
+            missing_index = list(np.where(np.sum(mask_test,axis=1)<num_of_tickers)[0])
+            non_missing = [j for j in range(time_stamp) if j not in missing_index]
+            replace_index = random.sample(non_missing,len(missing_index))
+            mask_test[replace_index,:] = mask_test[missing_index,:]
+            mask_test_all = mask_test
 
-        total_train = np.vstack((total_train,train_data))
-        total_val = np.vstack((total_val,val_data))
-        total_test = np.vstack((total_test,test_data))
+        else:
+            mask_test = np.where(np.isnan(test1.loc[start:end-1,tickers].to_numpy()),0,1)
+            missing_index = list(np.where(np.sum(mask_test,axis=1)<num_of_tickers)[0])
+            non_missing = [j for j in range(time_stamp) if j not in missing_index]
+            replace_index = random.sample(non_missing,len(missing_index))
+            mask_test[replace_index,:] = mask_test[missing_index,:]
 
-    
-    training_data = np.reshape(total_train,[-1,20,6])
-    validation_data = np.reshape(total_val,[-1,20,6])
-    testing_data = np.reshape(total_test,[-1,20,6])
-  
+            mask_test_all = np.concatenate([mask_test_all,mask_test]) 
 
-    np.save('./datasets/'+index_name+'/'+index_name+'_train.npy',training_data)
-    np.save('./datasets/'+index_name+'/'+index_name+'_val.npy',validation_data)
-    np.save('./datasets/'+index_name+'/'+index_name+'_test.npy',testing_data)
+    for i in range(test2.shape[0]//time_stamp):
+        start = i*time_stamp
+        end = (i+1)*time_stamp
+        mask_test = np.where(np.isnan(test2.loc[start:end-1,tickers].to_numpy()),0,1)
+        missing_index = list(np.where(np.sum(mask_test,axis=1)<num_of_tickers)[0])
+        non_missing = [j for j in range(time_stamp) if j not in missing_index]
+        replace_index = random.sample(non_missing,len(missing_index))
+        mask_test[replace_index,:] = mask_test[missing_index,:]
+
+        mask_test_all = np.concatenate([mask_test_all,mask_test]) 
+
+
+    mask_test_all=np.reshape(mask_test_all,[-1,time_stamp,num_of_tickers])        
+    np.save(data_path+'/'+index_name+'_mask_test.npy',mask_test_all)
 
     
 
@@ -247,5 +286,5 @@ if __name__ == "__main__":
     for index_name in ['DJ30','EU50','HSI']:
         data_download(index_name)
     for index_name in ['DJ30','EU50','HSI']:
-        process(index_name)
+        generate(index_name = index_name,data_path='./SSSD/datasets',time_stamp=30)
 
